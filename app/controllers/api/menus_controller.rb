b83e2a449c8c09167c773ef370500c5b3c9cda74
sjohnston92 <simonjohnston.c@gmail.com>
2020-09-21 14:34:12 -0600

    Both Rails and React have been made

--- app/controllers/api/menus_controller.rb
+++ app/controllers/api/menus_controller.rb
@@ -1,2 +1,36 @@
 class Api::MenusController < ApplicationController
+
+
+def index
+  redner json: Menu.all 
+end
+
+
+def create
+  item = Menu.new(menu_params)
+  if menu.save
+    render json: menu
+  else
+    render json: {errors: menu.errors}, status: :unprocessable_entity 
+  end
+  end
+end
+
+def update 
+  item = Menu.find(params[:id])
+  menu.update(complete: !menu.complete)
+  render json: menu
+end
+
+
+ def destroy
+  Menu.find(params[:id]).destroyrender json: {message: 'Menu deleted' } 
+  
+
+private
+
+def menu_params
+  params.require(:menu).permit(:name, :time)
+end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+10.times do |i|
+  Menu.create(name: "Menu-#{i}", time: i.odd?)
+end
